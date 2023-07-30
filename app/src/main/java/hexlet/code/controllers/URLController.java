2d6431e5a053c29af38395762acabe0fb979cbc6
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-07-30 17:56:49 +0200

    fix: add correct dependency for Handler class. Also add a Handler for creating new url in DB (still in progress)

--- app/src/main/java/hexlet/code/controllers/URLController.java
+++ app/src/main/java/hexlet/code/controllers/URLController.java
@@ -1,10 +1,40 @@
 package hexlet.code.controllers;
 
-import java.util.logging.Handler;
+import hexlet.code.URL;
+import hexlet.code.query.QURL;
+import io.javalin.http.Handler;
 
 public class URLController {
 
     public static Handler createURL = ctx -> {
-        String URL = ctx.
-    }
+        String urlName = ctx.formParam("name");
+
+        URL urlToAdd = new URL(urlName);
+
+        if (urlName.isEmpty() || urlName == null) {
+            ctx.sessionAttribute("flash", "Некорректный URL");
+            ctx.sessionAttribute("flash-type", "danger");
+            ctx.attribute("url", urlToAdd);
+            ctx.render("templates/index.html");
+            return;
+        }
+
+        URL urlToCheck = new QURL()
+                .name.iequalTo(urlName)
+                .findOne();
+
+        if (urlToAdd.getName().equals(urlToCheck.getName())) {
+            ctx.sessionAttribute("flash", "Страница уже существует");
+            ctx.sessionAttribute("flash-type", "info");
+            ctx.redirect("/urls");
+            return;
+        }
+
+        urlToAdd.save();
+
+        ctx.sessionAttribute("flash", "Страница успешно добавлена");
+        ctx.sessionAttribute("flash-type", "success");
+        ctx.redirect("/urls");
+
+    };
 }
