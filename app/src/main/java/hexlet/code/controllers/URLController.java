d725f4c3af1708bd3fbcd3837039f945633e9b6a
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-08-02 17:51:50 +0200

    fix: correct method render() in showURLs Handler, also refactor code by extracting some of its parts into separate private methods

--- app/src/main/java/hexlet/code/controllers/URLController.java
+++ app/src/main/java/hexlet/code/controllers/URLController.java
@@ -1,12 +1,13 @@
 package hexlet.code.controllers;
 
-import hexlet.code.URL;
-import hexlet.code.query.QURL;
+import hexlet.code.URLEntity;
+import hexlet.code.query.QURLEntity;
 import io.ebean.PagedList;
 import io.javalin.http.Handler;
 import io.javalin.http.NotFoundResponse;
-import org.objectweb.asm.Handle;
-
+import org.jetbrains.annotations.NotNull;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -15,50 +16,56 @@ public class URLController {
     private static final int ROWS_PER_PAGES = 10;
 
     public static Handler createURL = ctx -> {
+
         String urlName = ctx.formParam("url");
 
         System.out.println("urlName is: " + urlName);
 
-        URL urlToAdd = new URL(urlName);
-
-        if (urlName.isEmpty() || urlName == null) {
+        URL rawURL;
+        try {
+            rawURL = new URL(urlName);
+        } catch (MalformedURLException e) {
             ctx.sessionAttribute("flash", "Некорректный URL");
             ctx.sessionAttribute("flash-type", "danger");
-            ctx.attribute("url", urlToAdd);
-            ctx.render("templates/index.html");
+            ctx.redirect("templates/index.html");
             return;
         }
 
-        URL urlToCheck = new QURL()
-                .name.iequalTo(urlName)
-                .findOne();
+        String protocol = rawURL.getProtocol();
+        String host = rawURL.getHost();
+        int port = rawURL.getPort();
 
-        if (urlToCheck != null) {
+        String normalizedUrl = getNormalizedUrl(protocol, host, port);
+
+        System.out.println("normalizedUrl is: " + normalizedUrl);
+
+        if (doesUrlAlreadyExist(normalizedUrl)) {
             ctx.sessionAttribute("flash", "Страница уже существует");
             ctx.sessionAttribute("flash-type", "info");
             ctx.redirect("/urls");
             return;
         }
 
+        URLEntity urlToAdd = new URLEntity(normalizedUrl);
         urlToAdd.save();
 
         ctx.sessionAttribute("flash", "Страница успешно добавлена");
         ctx.sessionAttribute("flash-type", "success");
         ctx.redirect("/urls");
-
     };
 
     public static Handler showURLs = ctx -> {
+
         int page = ctx.queryParamAsClass("page", Integer.class).getOrDefault(1) - 1;
 
-        PagedList<URL> pagedURLs = new QURL()
+        PagedList<URLEntity> pagedURLs = new QURLEntity()
                 .setFirstRow(page * ROWS_PER_PAGES)
                 .setMaxRows(ROWS_PER_PAGES)
                 .orderBy()
-                    .id.asc()
+                .id.asc()
                 .findPagedList();
 
-        List<URL> URLs = pagedURLs.getList();
+        List<URLEntity> URLs = pagedURLs.getList();
 
         int currentPage = pagedURLs.getPageIndex() + 1;
         int lastPage = pagedURLs.getTotalPageCount() + 1;
@@ -67,17 +74,17 @@ public class URLController {
                 .boxed()
                 .toList();
 
-
         ctx.attribute("urls", URLs);
         ctx.attribute("pages", pages);
         ctx.attribute("currentPage", currentPage);
-        ctx.redirect("/urls/index.html");
+
+        ctx.render("urls/index.html");
     };
 
-    public static Handler showURL = ctx -> {
+    public static Handler showURLById = ctx -> {
         int id = ctx.pathParamAsClass("id", Integer.class).getOrDefault(null);
 
-        URL url = new QURL()
+        URLEntity url = new QURLEntity()
                 .id.equalTo(id)
                 .findOne();
 
@@ -89,4 +96,25 @@ public class URLController {
         ctx.render("urls/show.html");
 
     };
+
+    @NotNull
+    private static String getNormalizedUrl(String protocol, String host, int port) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(protocol).append("://").append(host);
+
+        if (port != -1) {
+            sb.append(port);
+        }
+
+        String normalizedUrl = sb.toString();
+        return normalizedUrl;
+    }
+
+    private static boolean doesUrlAlreadyExist(String urlToCheck) {
+        URLEntity urlFromDB = new QURLEntity()
+                .name.iequalTo(urlToCheck)
+                .findOne();
+
+        return urlFromDB != null;
+    }
 }
