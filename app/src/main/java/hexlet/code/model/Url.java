d1a457ae240aa2710c4c293f6f5e33d0ca521c80
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-08-08 13:12:51 +0200

    fix: add decorative changes to the code (delete an empty line)

--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -2,7 +2,6 @@ package hexlet.code.model;
 
 import io.ebean.Model;
 import io.ebean.annotation.WhenCreated;
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.time.Instant;
