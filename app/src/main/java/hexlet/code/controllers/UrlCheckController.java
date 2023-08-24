932715fbb5145a8eb7ba3d4f8ac3a8d2de076323
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-08-24 22:02:31 +0200

    fix: change UrlCheckController. It now correctly redirect user to /urls page after adding new url to the DB

--- app/src/main/java/hexlet/code/controllers/UrlCheckController.java
+++ app/src/main/java/hexlet/code/controllers/UrlCheckController.java
@@ -46,8 +46,10 @@ public class UrlCheckController {
             url.getUrlChecks().add(urlCheckToAdd);
             url.save();
 
+            ctx.redirect("/urls");
             ctx.sessionAttribute("flash", "Страница успешно проверена");
             ctx.sessionAttribute("flash-type", "success");
+
             LOGGER.info("Check is done and added to the DB");
         } catch (UnirestException e) {
             ctx.sessionAttribute("flash", "Некорректный адрес");
