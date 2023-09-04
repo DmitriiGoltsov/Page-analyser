81b36f943969db89594b4e4bb0828d3a1ee18fc6
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-09-04 14:26:41 +0200

    feat: created a special getter for the filed createdAt that returns Instant but not Timestamp type. It allows Thymeleaf to show time of creation correctly.

--- app/src/main/java/hexlet/code/model/UrlCheck.java
+++ app/src/main/java/hexlet/code/model/UrlCheck.java
@@ -3,33 +3,50 @@ package hexlet.code.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import java.sql.Timestamp;
 import java.time.Instant;
 
-@Getter
-@Setter
 @ToString
 public final class UrlCheck {
 
+    @Getter
+    @Setter
     private Long id;
 
+    @Getter
+    @Setter
     private int statusCode;
 
+    @Getter
+    @Setter
     private String title;
 
+    @Getter
+    @Setter
     private String h1;
 
+    @Getter
+    @Setter
     private String description;
 
-    private Instant createdAt;
+    @Getter
+    @Setter
+    private Timestamp createdAt;
 
+    @Getter
+    @Setter
     private Long urlId;
 
-
-    public UrlCheck(int statusCode, String title, String h1, String description, Instant createdAt) {
+    public UrlCheck(int statusCode, String title, String h1, String description, Timestamp createdAt, Long urlId) {
         this.statusCode = statusCode;
         this.title = title;
         this.h1 = h1;
         this.description = description;
         this.createdAt = createdAt;
+        this.urlId = urlId;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt.toInstant();
     }
 }
