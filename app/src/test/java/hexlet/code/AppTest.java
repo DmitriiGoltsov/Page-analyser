5371c7e30d0f83b1b7dfdacdd420ce12c5d67931
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-09-04 14:28:37 +0200

    feat: make all test methods to use repository pattern instead of ebean. Unused imports were deleted.

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -2,21 +2,27 @@ package hexlet.code;
 
 import hexlet.code.model.Url;
 import hexlet.code.model.UrlCheck;
-import hexlet.code.model.query.QUrl;
-import hexlet.code.model.query.QUrlCheck;
-import io.ebean.DB;
-import io.ebean.Database;
+import hexlet.code.repository.UrlCheckRepository;
+import hexlet.code.repository.UrlRepository;
+
 import io.javalin.Javalin;
 import jakarta.servlet.http.HttpServletResponse;
+
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
+import org.h2.engine.Database;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Assertions;
-import java.util.Objects;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.format.DateTimeFormatter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -35,12 +41,11 @@ public final class AppTest {
     private static Database database;
 
     @BeforeAll
-    public static void beforeAll() {
+    public static void beforeAll() throws SQLException, IOException {
         app = App.getApp();
         app.start(0);
         int port = app.port();
         baseUrl = "http://localhost:" + port;
-        database = DB.getDefault();
     }
 
     @AfterAll
@@ -49,10 +54,12 @@ public final class AppTest {
     }
 
     @BeforeEach
-    public void beforeEach() {
-        database.script().run("/truncate.sql");
-        Url url = new Url(CORRECT_URL);
-        url.save();
+    public void beforeEach() throws SQLException {
+        UrlRepository.truncateDB();
+        UrlCheckRepository.truncateDB();
+
+        Url firstUrl = new Url(CORRECT_URL, new Timestamp(System.currentTimeMillis()));
+        UrlRepository.save(firstUrl);
     }
 
     @Test
@@ -110,27 +117,30 @@ public final class AppTest {
         }
 
         @Test
-        public void testShowUrlById() {
+        public void testShowUrlById() throws SQLException {
 
-            Url actualUrl = new QUrl()
-                    .name.equalTo(CORRECT_URL)
-                    .findOne();
+            Url actualUrl = UrlRepository.findByName(CORRECT_URL).orElseThrow(
+                    () -> new SQLException("url with the name " + CORRECT_URL + " was not found!"));
 
-            long id = actualUrl.getId();
+            Long id = actualUrl.getId();
 
             HttpResponse<String> response = Unirest.get(baseUrl + "/urls/" + id).asString();
             String body = response.getBody();
 
             assertThat(response.getStatus()).isEqualTo(HttpServletResponse.SC_OK);
-            assertThat(body).contains(CORRECT_URL);
+            assertThat(body).contains(CORRECT_URL,
+                    actualUrl.getCreatedAt()
+                            .toLocalDateTime()
+                            .format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")));
 
-            Url wrongUrl = new Url(URL_FOR_NON_EXISTING_ENTITY_TEST);
-            wrongUrl.save();
-            long idForDeletion = Objects.requireNonNull(new QUrl()
-                            .name.eq(URL_FOR_NON_EXISTING_ENTITY_TEST)
-                            .findOne()).getId();
+            Url wrongUrl = new Url(URL_FOR_NON_EXISTING_ENTITY_TEST, new Timestamp(System.currentTimeMillis()));
+            UrlRepository.save(wrongUrl);
+            Long idForDeletion = UrlRepository.findByName(URL_FOR_NON_EXISTING_ENTITY_TEST)
+                    .orElseThrow(() -> new SQLException("wrongUrl with name " + URL_FOR_NON_EXISTING_ENTITY_TEST
+                                    + " was not found in DB!"))
+                    .getId();
 
-            wrongUrl.delete();
+            UrlRepository.delete(idForDeletion);
             response = Unirest.get(baseUrl + "/urls/" + idForDeletion).asString();
             assertThat(response.getStatus()).isEqualTo(HttpServletResponse.SC_NOT_FOUND);
         }
@@ -140,39 +150,31 @@ public final class AppTest {
     class UrlCheckControllerTest {
 
         @Test
-        public void addUrlCheckTest() {
+        public void addUrlCheckTest() throws SQLException {
 
             Unirest.post(baseUrl + "/urls")
                     .field("url", CORRECT_URL)
                     .asEmpty();
 
-            Url actualUrl = new QUrl()
-                    .name.equalTo(CORRECT_URL)
-                    .findOne();
+            Url actualUrl = UrlRepository.findByName(CORRECT_URL)
+                    .orElseThrow(() -> new SQLException("Url with name " + CORRECT_URL + " was not found"));
 
-            long id = actualUrl.getId();
+            Long id = actualUrl.getId();
 
-            assertThat(actualUrl).isNotNull();
             assertThat(actualUrl.getName()).isEqualTo(CORRECT_URL);
 
             String urlForPostMethod = baseUrl + "/urls/" + id + "/checks";
 
             Unirest.post(urlForPostMethod).asEmpty();
 
-            UrlCheck actualCheckUrl = new QUrlCheck()
-                    .url.equalTo(actualUrl)
-                    .createdAt.desc()
-                    .findOne();
+            UrlCheck actualCheckUrl = UrlCheckRepository.findLastCheckByUrlId(id)
+                    .orElseThrow(() -> new SQLException("The last check for url with id " + id + " was not found"));
 
-            assertThat(actualCheckUrl).isNotNull();
             assertThat(actualCheckUrl.getStatusCode()).isEqualTo(200);
             assertThat(actualCheckUrl.getTitle()).isEqualTo("Google");
             assertThat(actualCheckUrl.getH1())
                     .isEqualTo("");
             assertThat(actualCheckUrl.getDescription()).contains("");
         }
-
     }
-
-
 }
